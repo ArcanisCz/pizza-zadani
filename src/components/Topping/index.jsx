c4b2ceb7fd7de5b7aeffdac794a035dbd2fe64c6
Jitka Hladká <jitka.hladka@gmail.com>
2022-10-22 23:54:06 +0200

    State lifting

--- src/components/Topping/index.jsx
+++ src/components/Topping/index.jsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import Check from "../Check";
 import './style.css';
 
-const Topping = ({ topping }) => {
+const Topping = ({ topping, onCheck, checked }) => {
+
   return (
     <div className="topping">
-      <Check />
+      <Check onChange={onCheck} checked={checked}/>
       <span className="topping__content">
         {topping.name}: {topping.price} €
       </span>
--- src/components/ToppingsSelect/index.jsx
+++ src/components/ToppingsSelect/index.jsx
@@ -3,14 +3,35 @@ import Topping from "../Topping";
 import './style.css';
 
 const ToppingsSelect = ({ toppings }) => {
+
+  const [toppingsToChoose, setToppingsToChoose] = useState(toppings);
+
+  const handleSelect = (index, isChecked) => {
+    const newToppings = [...toppings];
+    newToppings[index].selected = isChecked;
+    setToppingsToChoose(newToppings);
+      
+    };
+
+  const selectedToppings = toppingsToChoose.filter((topping) => topping.selected === true)
+
+  let totalCount = selectedToppings.length;
+  let totalPrice = 0;
+
+  selectedToppings.forEach((topping) => {
+    totalPrice += topping.price; 
+  });
+
+  
+
   return (
     <>
       <p>Choose as many toppings as you want</p>
-      <p>Selected toppings: 0, total price: 0 Euro</p>
+      <p>Selected toppings: {totalCount}, total price: {totalPrice.toFixed(2)} Euro</p>
         
-      <div class="toppings">
-        {toppings.map((topping) => (
-          <Topping topping={topping} key={topping.name} />
+      <div className="toppings">
+        {toppings.map((topping, index) => (
+          <Topping topping={topping} key={topping.name} checked={topping.selected} onCheck={(isChecked) => handleSelect(index, isChecked)}/>
         ))}
       </div>
     </>
