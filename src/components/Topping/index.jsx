aff0e346dba553e50a3301fd743bea0fb11c3e7f
Sabina Jarabkova <sabina@MacBook-Air-uzivatela-Sabina.local>
2022-10-28 21:55:30 +0200

    pizza ukol - vypracovane - uz iba divne cislo pri spocitavani EUR

--- src/components/Topping/index.jsx
+++ src/components/Topping/index.jsx
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import Check from "../Check";
 import './style.css';
 
-const Topping = ({ topping }) => {
+const Topping = ({ topping, onToppingChange}) => {
+
+
+
   return (
     <div className="topping">
-      <Check />
+      <Check checked={topping.selected} onChange={onToppingChange} />
       <span className="topping__content">
         {topping.name}: {topping.price} €
       </span>
--- src/components/ToppingsSelect/index.jsx
+++ src/components/ToppingsSelect/index.jsx
@@ -3,16 +3,41 @@ import Topping from "../Topping";
 import './style.css';
 
 const ToppingsSelect = ({ toppings }) => {
+
+  const [toppingCheck, setToppingCheck] = useState(toppings);
+
+  const handleToppingChange = (index, selected) => {
+    const newToppingCheck = [...toppingCheck];
+    newToppingCheck[index].selected = selected;
+    setToppingCheck(newToppingCheck);
+  };
+
+  let toppingCount = 0;
+  toppingCheck.forEach((topping) => toppingCount += topping.selected);
+
+  let toppingPrice = 0;
+  toppingCheck.forEach((topping) => {
+    let newPrice = topping.price;
+    if ( topping.selected === true) {
+      toppingPrice += newPrice
+    }}
+  )
+
+
   return (
     <>
       <p>Choose as many toppings as you want</p>
-      <p>Selected toppings: 0, total price: 0 Euro</p>
+      <p>Selected toppings: {toppingCount}, total price: {toppingPrice} Euro</p>
         
-      <div class="toppings">
-        {toppings.map((topping) => (
-          <Topping topping={topping} key={topping.name} />
+      <div className="toppings">
+        {toppings.map((topping, index) => (
+          <Topping 
+            topping={topping} 
+            key={topping.name} 
+            onToppingChange={(selected) => handleToppingChange(index, selected)} />
         ))}
       </div>
+
     </>
   );
 };
